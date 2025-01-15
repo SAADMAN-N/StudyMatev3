6b72e77c312bcb75d2f59d5cd6dd22fd36f0f746
Sharfuzzaman Noor Sadman <sadmansh@msu.edu>
2025-01-15 03:40:04 -0500

    Add reliable TURN server configuration

--- src/lib/webrtc.ts
+++ src/lib/webrtc.ts
@@ -44,7 +44,25 @@ export class WebRTCManager {
         trickle: true,
         config: {
           iceServers: [
-            { urls: 'stun:stun.l.google.com:19302' }
+            {
+              urls: [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+                'stun:stun3.l.google.com:19302',
+                'stun:stun4.l.google.com:19302'
+              ]
+            },
+            {
+              urls: 'turn:openrelay.metered.ca:80',
+              username: 'openrelayproject',
+              credential: 'openrelayproject'
+            },
+            {
+              urls: 'turn:openrelay.metered.ca:443',
+              username: 'openrelayproject',
+              credential: 'openrelayproject'
+            }
           ]
         }
       });
